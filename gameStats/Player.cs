5e69d93808309173f27d94e50c446ac9be31631c
SebastianPetak <sebastianpetak@gmail.com>
2018-07-16 07:42:34 -0500

    Add & Serialize Player Data

---
+++ gameStats/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameStats
+{
+
+    public class RootObject
+    {
+        public Player[] Player { get; set; }
+    }
+
+    public class Player
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public int goals_scored { get; set; }
+        public string team_name { get; set; }
+        public string favorite_percentage { get; set; }
+        public string photo { get; set; }
+        public bool good_player { get; set; }
+    }
+
+}
